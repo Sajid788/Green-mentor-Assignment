2a3633ebce6a320ff1b7b922f2af90625497b785
Md Sajid Ansari <mdsajidalam8340@gmail.com>
2024-03-27 15:06:57 +0530

    all done

--- backend/model/task.model.js
+++ backend/model/task.model.js
@@ -1,10 +1,22 @@
 const mongoose = require("mongoose");
 
 const noteSchema = new mongoose.Schema({
-  title: { type: String, required: true },
-  description: { type: String, required: true },
-  status: {type: String,enum: ["Pending", "InProgress", "Completed"],default: "Pending",},
-  createrId: { type: mongoose.Types.ObjectId, },
+  title: {
+    type: String,
+    required: true,
+    minlength: [2, 'Tittle must be at least 2 characters'],
+    maxlength: [100, 'Title cannot axceed 100 character'],
+  },
+  description: { type: String, required: true,
+    minlength: [10, 'Description must be at least 10 characters'],
+    maxlength: [800, 'Description cannot axceed 800 character'],
+   },
+  status: {
+    type: String,
+    enum: ["Pending", "InProgress", "Completed"],
+    default: "Pending",
+  },
+  createrId: { type: mongoose.Types.ObjectId },
 });
 
 const TaskModel = mongoose.model("Tasks", noteSchema);
